2f595eb15fbb91399ac4082c67d5090f5b303781
realeve <realeve@qq.com>
2020-07-16 10:48:46 +0800

    格式化代码完成

--- ftp.php
+++ ftp.php
@@ -1,56 +1,57 @@
 <?php
-include 'ftp_lib.php'; 
+include 'ftp_lib.php';
 
-/*	libin 
-	2020-07-15: 上传PDF文件至MES服务器；
+/*	libin
+    2020-07-15: 上传PDF文件至MES服务器；
 */
 
 $config = [
-	'host'=>'mes.cdyc.cbpm',
-	'user'=>'ftper',
-	'pass'=>'5guang10se',
-	'port'=>'2001'
-]; 
-$dirName = "nepal/";
+    'host'=>'mes.cdyc.cbpm',
+    'user'=>'ftper',
+    'pass'=>'5guang10se',
+    'port'=>'2001'
+];
+$dirName = "nepal/" ;
 
-function createNonceStr($length = 32) {
-	$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	$str = "";
-	for ($i = 0; $i < $length; $i++) {
-	$str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
-	}
-	return $str;
+function createNonceStr($length = 32)
+{
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $str = "";
+    for ($i = 0; $i < $length; $i++) {
+        $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+    }
+    return $str;
 }
 
-function getFtp(){ 
-	global $config;
+function getFtp()
+{
+    global $config;
     $ftp = new FtpConn($config);
     $result = $ftp->connect();
-    if (!$result){
+    if (!$result) {
         handleErr($ftp->get_error_msg());
         return false;
-    }  
-	return $ftp; 
+    }
+    return $ftp;
 }
 
 function handlePost()
-{ 
-	global $dirName;
-	/*
-	$conn_id = ftp_connect('10.9.3.5',2001);  
-	$login_result = ftp_login($conn_id, 'ftper', '5guang10se');
-	*/
- 
-	$ftp = getFtp();
-	if(!$ftp){
-		return;
-	} 
-	if(!$_FILES || !$_FILES["file"])
-	{
-		echo '{"status":"0","msg":"未读取到文件"}';
-		return;
-	}
-	
+{
+    global $dirName;
+    /*
+    $conn_id = ftp_connect('10.9.3.5',2001);
+    $login_result = ftp_login($conn_id, 'ftper', '5guang10se');
+    */
+
+    $ftp = getFtp();
+    if (!$ftp) {
+        return;
+    }
+    if (!$_FILES || !$_FILES["file"]) {
+        echo '{"status":"0","msg":"未读取到文件"}';
+        return;
+    }
+
     // 500M大小限制
     if ($_FILES["file"]["size"] < 1024 * 1024 * 500) {
         if ($_FILES["file"]["error"] > 0) {
@@ -58,82 +59,79 @@ function handlePost()
         } else {
             $file = $_FILES["file"];
             $name = $file["name"];
-			$filename = $file["name"];
-			
-			if(strpos($filename,'.')==-1){
-				$fileType = '';
-				$arr = explode('/',$file['type']);
-				$fileType = '.'.$arr[count($arr)-1]; 
-				$filename = time().'_'.createNonceStr(8).$fileType;
-			} 
-			
-            $remote_file = $dirName.$filename; 
+            $filename = $file["name"];
+
+            if (strpos($filename, '.')==-1) {
+                $fileType = '';
+                $arr = explode('/', $file['type']);
+                $fileType = '.'.$arr[count($arr)-1];
+                $filename = time().'_'.createNonceStr(8).$fileType;
+            }
+
+            $remote_file = $dirName.$filename;
             $return['msg'] = "上传失败";
-            if ($ftp->upload($file['tmp_name'],$remote_file)){
+            if ($ftp->upload($file['tmp_name'], $remote_file)) {
                 $return['msg'] = "上传成功";
-            }   
+            }
             $return['url'] = $remote_file;
-            $return['status'] = 1; 
-            $return['name'] = $name; 
-			
-			// 关闭链接
-			$ftp->close();
-			 
+            $return['status'] = 1;
+            $return['name'] = $name;
+
+            // 关闭链接
+            $ftp->close();
+
             echo json_encode($return);
         }
     }
 }
- 
 
 function handleErr($error='上传文件失败')
 {
-    $return['status'] = 0; 
+    $return['status'] = 0;
     $return['msg'] = $error;
     echo json_encode($return);
 }
 
 // 删除文件
 // URL: http://10.8.1.25/ftp?name=NRB10_B82000001_20200715.pdf
-function handleGet(){
-  
-  global $dirName;
-  if(isset($_GET['name'])){
-	  
-	$ftp = getFtp();
-	if(!$ftp){
-		return;
-	}  
-	$filename = $dirName.$_GET['name']; 
-	if ($ftp->delete_file($filename)){
-		$return['status'] = 1;
-		$return['msg'] = '文件删除成功'; 
-	}else{
-		$return['status'] = 0;
-		$return['msg'] = '文件'.$filename.'删除失败';
-	}    
-	$ftp->close();
-	
-  }else{
-	  $return['status'] = 0;
-	  $return['msg'] = '请求参数错误，需要指定name字段';
-  }
-  
-  if(isset($_GET['callback'])){
-	echo $_GET['callback'].'('.json_encode($return).')';
-  }else{
-	echo json_encode($return);
-  }  
+function handleGet()
+{
+    global $dirName;
+    if (isset($_GET['name'])) {
+        $ftp = getFtp();
+        if (!$ftp) {
+            return;
+        }
+        $filename = $dirName.$_GET['name'];
+        if ($ftp->delete_file($filename)) {
+            $return['status'] = 1;
+            $return['msg'] = '文件删除成功';
+        } else {
+            $return['status'] = 0;
+            $return['msg'] = '文件'.$filename.'删除失败';
+        }
+        $ftp->close();
+    } else {
+        $return['status'] = 0;
+        $return['msg'] = '请求参数错误，需要指定name字段';
+    }
+
+    if (isset($_GET['callback'])) {
+        echo $_GET['callback'].'('.json_encode($return).')';
+    } else {
+        echo json_encode($return);
+    }
 }
-	
+
 function init()
 {
     $requestType = $_SERVER['REQUEST_METHOD'];
-    // 指定允许其他域名访问 // 正式布署请设置为前端资源主站 
-	
+    // 指定允许其他域名访问 // 正式布署请设置为前端资源主站
+
     // 响应类型
     header('Access-Control-Allow-Methods:GET,POST,PUT,OPTIONS');
-    header('Access-Control-Allow-Headers:x-requested-with,content-type'); 
-	
+    header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
     if ($requestType == "OPTIONS") {
         header('Access-Control-Allow-Credentials:true');
         header('Access-Control-Max-Age:1728000');
@@ -141,17 +139,16 @@ function init()
         header('Content-Length: 0', true);
         header("status: 204");
         header("HTTP/1.0 204 No Content");
-    }else{
-		header("Content-type: application/json");
-		if ($requestType == "POST") {		
-			handlePost();
-		}else if($requestType == "GET"){     
-			handleGet();		  
-		} else {
-			handleErr();
-		} 
-	}
-	
+    } else {
+        header("Content-type: application/json");
+        if ($requestType == "POST") {
+            handlePost();
+        } elseif ($requestType == "GET") {
+            handleGet();
+        } else {
+            handleErr();
+        }
+    }
 }
 
 init();
--- ftp_lib.php
+++ ftp_lib.php
@@ -7,7 +7,8 @@
  * @author sunjiaqiang
  * @email 1355049422@qq.com
  */
-class FtpConn{
+class FtpConn
+{
     private $host='';//远程服务器地址
     private $user='';//ftp用户名
     private $pass='';//ftp密码
@@ -22,18 +23,19 @@ class FtpConn{
      */
     public function __construct(array $config=[])
     {
-        empty($config) OR $this->initialize($config);
+        empty($config) or $this->initialize($config);
     }
 
     /**
      * 初始化数据
      * @param array $config 配置文件数组
      */
-    public function initialize(array $config=[]){
+    public function initialize(array $config=[])
+    {
         $this->host = $config['host'];
         $this->user = $config['user'];
         $this->pass = $config['pass'];
-        $this->port = isset($config['port']) ? $config['port'] : 21;
+        $this->port = $config['port'] ?? 21;
     }
 
     /**
@@ -41,17 +43,18 @@ class FtpConn{
      * @param array $config 配置文件数组
      * @return bool
      */
-    public function connect(array $config=[]){
-        empty($config) OR $this->initialize($config);
-        if (FALSE == ($this->conn = @ftp_connect($this->host,$this->port))){
+    public function connect(array $config=[])
+    {
+        empty($config) or $this->initialize($config);
+        if (false == ($this->conn = @ftp_connect($this->host, $this->port))) {
             $this->error = "主机连接失败:".$this->host;
-            return FALSE;
+            return false;
         }
-        if ( ! $this->_login()){
+        if (! $this->_login()) {
             $this->error = "服务器登录失败";
-            return FALSE;
+            return false;
         }
-        return TRUE;
+        return true;
     }
 
     /**
@@ -61,24 +64,25 @@ class FtpConn{
      * @param bool $permissions 文件夹权限
      * @param string $mode 上传模式(ascii和binary其中之一)
      */
-    public function upload($local_file='',$remote_file='',$mode='auto',$permissions=NULL){
-        if ( ! file_exists($local_file)){
+    public function upload($local_file='', $remote_file='', $mode='auto', $permissions=null)
+    {
+        if (! file_exists($local_file)) {
             $this->error = "本地文件不存在";
-            return FALSE;
+            return false;
         }
-        if ($mode == 'auto'){
+        if ($mode == 'auto') {
             $ext = $this->_get_ext($local_file);
             $mode = $this->_set_type($ext);
         }
         //创建文件夹
         $this->_create_remote_dir($remote_file);
         $mode = ($mode == 'ascii') ? FTP_ASCII : FTP_BINARY;
-        $result = @ftp_put($this->conn,$remote_file,$local_file,$mode);//同步上传
-        if ($result === FALSE){
+        $result = @ftp_put($this->conn, $remote_file, $local_file, $mode);//同步上传
+        if ($result === false) {
             $this->error = "文件上传失败";
-            return FALSE;
+            return false;
         }
-        return TRUE;
+        return true;
     }
 
     /**
@@ -87,59 +91,59 @@ class FtpConn{
      * @param string $remote_file 远程文件地址
      * @param string $mode 上传模式(ascii和binary其中之一)
      */
-    public function download($local_file='',$remote_file='',$mode='auto'){
-        if ($mode == 'auto'){
+    public function download($local_file='', $remote_file='', $mode='auto')
+    {
+        if ($mode == 'auto') {
             $ext = $this->_get_ext($remote_file);
             $mode = $this->_set_type($ext);
         }
         $mode = ($mode == 'ascii') ? FTP_ASCII : FTP_BINARY;
         $result = @ftp_get($this->conn, $local_file, $remote_file, $mode);
-        if ($result === FALSE){
-            return FALSE;
+        if ($result === false) {
+            return false;
         }
-        return TRUE;
+        return true;
     }
 
     /**
      * 删除ftp服务器端文件
      * @param string $remote_file 文件地址
      */
-    public function delete_file($remote_file=''){
-        $result = @ftp_delete($this->conn,$remote_file); 
-        if ($result === FALSE){
-            return FALSE;
+    public function delete_file($remote_file='')
+    {
+        $result = @ftp_delete($this->conn, $remote_file);
+        if ($result === false) {
+            return false;
         }
-        return TRUE;
+        return true;
     }
     /**
      * ftp创建多级目录
      * @param string $remote_file 要上传的远程图片地址
+     * @param null|mixed $permissions
      */
-    private function _create_remote_dir($remote_file='',$permissions=NULL){
+    private function _create_remote_dir($remote_file='', $permissions=null)
+    {
         $remote_dir = dirname($remote_file);
-        $path_arr = explode('/',$remote_dir); // 取目录数组
+        $path_arr = explode('/', $remote_dir); // 取目录数组
         //$file_name = array_pop($path_arr); // 弹出文件名
         $path_div = count($path_arr); // 取层数
-        foreach($path_arr as $val) // 创建目录
-        {
-            if(@ftp_chdir($this->conn,$val) == FALSE)
-            {
-                $tmp = @ftp_mkdir($this->conn,$val);//此处创建目录时不用使用绝对路径(不要使用:2018-02-20/ceshi/ceshi2，这种路径)，因为下面ftp_chdir已经已经把目录切换成当前目录
-                if($tmp == FALSE)
-                {
+        foreach ($path_arr as $val) { // 创建目录
+            if (@ftp_chdir($this->conn, $val) == false) {
+                $tmp = @ftp_mkdir($this->conn, $val);//此处创建目录时不用使用绝对路径(不要使用:2018-02-20/ceshi/ceshi2，这种路径)，因为下面ftp_chdir已经已经把目录切换成当前目录
+                if ($tmp == false) {
                     echo "目录创建失败，请检查权限及路径是否正确！";
                     exit;
                 }
-                if ($permissions !== NULL){
+                if ($permissions !== null) {
                     //修改目录权限
-                    $this->_chmod($val,$permissions);
+                    $this->_chmod($val, $permissions);
                 }
-                @ftp_chdir($this->conn,$val);
+                @ftp_chdir($this->conn, $val);
             }
         }
 
-        for($i=0;$i<$path_div;$i++) // 回退到根,因为上面的目录切换导致当前目录不在根目录
-        {
+        for ($i=0;$i<$path_div;$i++) { // 回退到根,因为上面的目录切换导致当前目录不在根目录
             @ftp_cdup($this->conn);
         }
     }
@@ -148,23 +152,24 @@ class FtpConn{
      * 递归删除ftp端目录
      * @param string $remote_dir ftp目录地址
      */
-    public function delete_dir($remote_dir=''){
+    public function delete_dir($remote_dir='')
+    {
         $list = $this->list_file($remote_dir);
-        if ( ! empty($list)){
+        if (! empty($list)) {
             $count = count($list);
-            for ($i=0;$i<$count;$i++){
-                if ( ! preg_match('#\.#',$list[$i]) && !@ftp_delete($this->conn,$list[$i])){
+            for ($i=0;$i<$count;$i++) {
+                if (! preg_match('#\.#', $list[$i]) && !@ftp_delete($this->conn, $list[$i])) {
                     //这是一个目录，递归删除
                     $this->delete_dir($list[$i]);
-                }else{
+                } else {
                     $this->delete_file($list[$i]);
                 }
             }
         }
-        if (@ftp_rmdir($this->conn,$remote_dir) === FALSE){
-            return FALSE;
+        if (@ftp_rmdir($this->conn, $remote_dir) === false) {
+            return false;
         }
-        return TRUE;
+        return true;
     }
 
     /**
@@ -172,20 +177,22 @@ class FtpConn{
      * @param string $old_file 旧文件/文件夹名
      * @param string $new_file 新文件/文件夹名
      */
-    public function remane($old_file='',$new_file=''){
-        $result = @ftp_rename($this->conn,$old_file,$new_file);
-        if ($result === FALSE){
+    public function remane($old_file='', $new_file='')
+    {
+        $result = @ftp_rename($this->conn, $old_file, $new_file);
+        if ($result === false) {
             $this->error = "移动失败";
-            return FALSE;
+            return false;
         }
-        return TRUE;
+        return true;
     }
 
     /**
      * 列出ftp指定目录
      * @param string $remote_path
      */
-    public function list_file($remote_path=''){
+    public function list_file($remote_path='')
+    {
         $contents = @ftp_nlist($this->conn, $remote_path);
         return $contents;
     }
@@ -194,17 +201,19 @@ class FtpConn{
      * 获取文件的后缀名
      * @param string $local_file
      */
-    private function _get_ext($local_file=''){
-        return (($dot = strrpos($local_file,'.'))==FALSE) ? 'txt' : substr($local_file,$dot+1);
+    private function _get_ext($local_file='')
+    {
+        return (($dot = strrpos($local_file, '.'))==false) ? 'txt' : substr($local_file, $dot+1);
     }
 
     /**
      * 根据文件后缀获取上传编码
      * @param string $ext
      */
-    private function _set_type($ext=''){
+    private function _set_type($ext='')
+    {
         //如果传输的文件是文本文件，可以使用ASCII模式，如果不是文本文件，最好使用BINARY模式传输。
-        return in_array($ext, ['txt', 'text', 'php', 'phps', 'php4', 'js', 'css', 'htm', 'html', 'phtml', 'shtml', 'log', 'xml'], TRUE) ? 'ascii' : 'binary';
+        return in_array($ext, ['txt', 'text', 'php', 'phps', 'php4', 'js', 'css', 'htm', 'html', 'phtml', 'shtml', 'log', 'xml'], true) ? 'ascii' : 'binary';
     }
 
     /**
@@ -212,32 +221,36 @@ class FtpConn{
      * @param $path 目录路径
      * @param int $mode 权限值
      */
-    private function _chmod($path,$mode=0755){
-        if (FALSE == @ftp_chmod($this->conn,$path,$mode)){
-            return FALSE;
+    private function _chmod($path, $mode=0755)
+    {
+        if (false == @ftp_chmod($this->conn, $path, $mode)) {
+            return false;
         }
-        return TRUE;
+        return true;
     }
 
     /**
      * 登录Ftp服务器
      */
-    private function _login(){
-        return @ftp_login($this->conn,$this->user,$this->pass);
+    private function _login()
+    {
+        return @ftp_login($this->conn, $this->user, $this->pass);
     }
 
     /**
      * 获取上传错误信息
      */
-    public function get_error_msg(){
+    public function get_error_msg()
+    {
         return $this->error;
     }
-    
+
     /**
      * 关闭ftp连接
      * @return bool
      */
-    public function close(){
-        return $this->conn ? @ftp_close($this->conn_id) : FALSE;
+    public function close()
+    {
+        return $this->conn ? @ftp_close($this->conn_id) : false;
     }
 }
